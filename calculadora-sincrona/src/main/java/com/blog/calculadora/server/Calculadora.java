6a5ea0f8f6990278f05cb400991a2155466f2411
Gabrielgsw <ggermanow279@gmail.com>
2025-10-31 16:39:09 -0300

    melhorias calc sincrona

--- calculadora-sincrona/src/main/java/com/blog/calculadora/server/Calculadora.java
+++ calculadora-sincrona/src/main/java/com/blog/calculadora/server/Calculadora.java
@@ -17,7 +17,7 @@ public class Calculadora extends UnicastRemoteObject implements CalculadoraOpera
 
     @Override
     public double soma(double x, double y) throws RemoteException {
-        System.out.println("Somando...");
+        System.out.println("Soma: "+x+"+"+y+" = "+(x+y));
         return x + y;
     }
 
@@ -25,46 +25,55 @@ public class Calculadora extends UnicastRemoteObject implements CalculadoraOpera
 
     @Override
     public double subtracao(double x, double y) throws RemoteException {
+        System.out.println("Subtração: "+x+"-"+y+" = "+(x-y));
         return x - y;
     }
 
     @Override
     public double multiplicacao(double x, double y) throws RemoteException {
+        System.out.println("Multiplicação: "+x+"*"+y+" = "+(x*y));
         return x * y;
     }
 
     @Override
     public double divisao(double x, double y) throws RemoteException {
+        System.out.println("Divisão: "+x+"/"+y+" = "+(x/y));
         return x/y;
     }
 
     @Override
     public double log(double x) throws RemoteException {
+        System.out.println("Logaritmo: "+x+" = "+Math.log10(x));
         return Math.log10(x);
     }
 
     @Override
     public double ln(double x) throws RemoteException {
+        System.out.println("Logaritmo neperiano: "+x+" = "+Math.log(x));
         return Math.log(x);
     }
 
     @Override
     public double exp(double x, double y) throws RemoteException {
+        System.out.println("Exponenciação: "+x+" elevado a "+y+" = "+(x*y));
         return Math.pow(x,y);
     }
 
     @Override
     public double seno(double x) throws RemoteException {
+        System.out.println("Seno de : "+x+" = "+Math.sin(x));
         return Math.sin(x);
     }
 
     @Override
     public double cosseno(double x) throws RemoteException {
+        System.out.println("Cosseno de : "+x+" = "+Math.cos(x));
         return Math.cos(x);
     }
 
     @Override
     public double raiz(double x) throws RemoteException {
-        return 0;
+        System.out.println("Raiz de: "+x+" = "+Math.sqrt(x));
+        return Math.sqrt(x);
     }
 }
