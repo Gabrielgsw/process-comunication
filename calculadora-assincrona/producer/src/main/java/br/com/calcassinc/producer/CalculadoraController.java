ccabbf2527ef96c26d7d8d7b2d35f4268609ebed
Gabrielgsw <ggermanow279@gmail.com>
2025-11-03 18:50:50 -0300

    feat: finalizando calculado assincrona

---
+++ calculadora-assincrona/producer/src/main/java/br/com/calcassinc/producer/CalculadoraController.java
@@ -0,0 +1,29 @@
+package br.com.calcassinc.producer;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/calc")
+public class CalculadoraController {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public CalculadoraController(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @PostMapping
+    public String sendTask(@RequestParam String operation, @RequestParam List<Double> numbers) {
+        String message = operation + ":" + numbers.toString();
+        rabbitTemplate.convertAndSend(RabbitConfig.QUEUE_NAME, message);
+        return "Tarefa enviada: " + message;
+    }
+
+    // opcional para teste rápido via navegador:
+    @GetMapping("/ping")
+    public String ping() {
+        return "API online!";
+    }
+}
